8007cd586e7f6ebc256bbf295ac1c4c5839aec94
Ganesh sai <ganeshsai1346@gmail.com>
2022-11-19 16:35:08 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -21,6 +21,16 @@ function App() {
 
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/calender" element={<Calender />} /> */}
             </Routes>
           </main>
         </div>
