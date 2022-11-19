e091aa6d1decc8407a02a6a1a107f66f8550d81e
Ganesh sai <ganeshsai1346@gmail.com>
2022-11-19 16:48:16 +0100

    changes made

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,4 +1,8 @@
 /** @format */
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
 
 const Sidebar = () => {
   return <div>Side bar</div>;
