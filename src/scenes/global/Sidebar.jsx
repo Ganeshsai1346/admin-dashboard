e297464d10e777cce3a20d2c9020692dac0c36f0
Ganesh sai <ganeshsai1346@gmail.com>
2022-11-21 22:51:09 +0100

    changes

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -3,9 +3,17 @@ import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Container, Row } from "react-bootstrap";
 
 const Sidebar = () => {
-  return <div>Side bar</div>;
+  return (
+    <div>
+      <Container>
+        <Row></Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Sidebar;
