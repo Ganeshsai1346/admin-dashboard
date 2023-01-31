a84bc1f006aaa7d9abe92cc6f679d2b86f673788
Ganesh sai <ganeshsai1346@gmail.com>
2023-01-31 22:14:27 +0100

    changes made

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-/** @format */
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -8,7 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,3 @@
-/** @format */
-
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
 
